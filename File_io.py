48aaad670e164ae5b263b9f03715f6dfcb0d1ec2
Anatoly-23411 <popov234111@gmail.com>
2024-09-09 15:08:36 +0500

    Добавлено автоматическое копирование ссылки в буфер обмена

--- File_io.py
+++ File_io.py
@@ -3,6 +3,7 @@ from tkinter import filedialog as fd
 from tkinter import messagebox as mb
 from tkinter import ttk
 import requests
+import pyperclip
 # from PyInstaller.loader.pyiboot01_bootstrap import entry
 # from Scripts.Cripta import window
 # from bottle import response, request
@@ -19,6 +20,8 @@ def upload():
                 link = response.json()["link"]
                 entry.delete(0, END)
                 entry.insert(0, link)
+                pyperclip.copy(link)
+                mb.showinfo("Ссылка скопирована",  f"Ссылка {link} успешно скопирована в буфер обмена")
     except Exception as e:
         mb.showerror("Ошибка", f"Произошла ошибка: {e}")
 
